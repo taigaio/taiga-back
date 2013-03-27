b2bbc933c7d3a75bc919ddea7555c1dc4055f3e3
Andrey Antukh <niwi@niwi.be>
2013-03-27 17:25:44 +0100

    Minor fixes on urls.

--- greenmine/urls.py
+++ greenmine/urls.py
@@ -6,8 +6,8 @@ admin.autodiscover()
 from greenmine.base.views import Login, Logout
 
 urlpatterns = patterns('',
-    url(r'^api/auth/login/$', Login.as_view(), namespace='api-login')),
-    url(r'^api/auth/logout/$', Logout.as_view(), namespace='api-logout')),
+    url(r'^api/auth/login/$', Login.as_view(), name='api-login'),
+    url(r'^api/auth/logout/$', Logout.as_view(), name='api-logout'),
     url(r'^api/$', 'greenmine.base.views.api_root'),
     url(r'^api/scrum/', include('greenmine.scrum.urls')),
     url(r'^admin/', include(admin.site.urls)),
