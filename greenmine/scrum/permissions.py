5da1a7e1c888c26e6918831893840cd43f4bb0c1
Andrey Antukh <niwi@niwi.be>
2013-04-24 12:27:30 +0200

    Change permission name for issue delete action.

--- greenmine/scrum/permissions.py
+++ greenmine/scrum/permissions.py
@@ -41,7 +41,7 @@ class IssueDetailPermission(BaseDetailPermission):
     get_permission = "can_view_issue"
     put_permission = "change_issue"
     patch_permission = "change_issue"
-    delete_permission = "can_delete_issue"
+    delete_permission = "delete_issue"
     safe_methods = ['HEAD', 'OPTIONS']
     path_to_project =  ['project']
 
