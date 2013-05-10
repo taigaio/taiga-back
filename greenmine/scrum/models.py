7d3dc0f28ac0136f4a3932fc3301f2e1a60f7f9f
Jesús Espino <jesus.espino@kaleidos.net>
2013-05-10 14:04:40 +0200

    Changing the usage of now to use the timezone now

--- greenmine/scrum/models.py
+++ greenmine/scrum/models.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
-
 from django.db import models
 from django.utils import timezone
 from django.dispatch import receiver
@@ -646,9 +644,9 @@ def tasks_close_handler(sender, instance, **kwargs):
 
     if instance.id:
         if sender.objects.get(id=instance.id).status.is_closed == False and instance.status.is_closed == True:
-            instance.finished_date = datetime.now()
+            instance.finished_date = timezone.now()
             if all([task.status.is_closed for task in instance.user_story.tasks.exclude(id=instance.id)]):
-                instance.user_story.finish_date = datetime.now()
+                instance.user_story.finish_date = timezone.now()
                 instance.user_story.save()
         elif sender.objects.get(id=instance.id).status.is_closed == True and instance.status.is_closed == False:
             instance.finished_date = None
