29e01ab226f5451e22ba3291e81bbaff13ce1867
David Barragán Merino <david.barragan@kaleidos.net>
2013-07-16 10:53:18 +0200

    Smallfix: Send more print message to sys.stderr

--- greenmine/settings/__init__.py
+++ greenmine/settings/__init__.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-
-from __future__ import absolute_import
-import os
+from __future__ import (
+    absolute_import,
+    print_function
+)
+import os, sys
 
 try:
-    print "Trying import local.py settings..."
+    print("Trying import local.py settings...", file=sys.stderr)
     from .local import *
 except ImportError:
-    print "Trying import development.py settings..."
+    print("Trying import development.py settings...", file=sys.stderr)
     from .development import *
