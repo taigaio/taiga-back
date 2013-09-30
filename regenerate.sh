775cfb29333249dafe0638aef5877cbbbcd1d05f
David Barragán Merino <david.barragan@kaleidos.net>
2013-09-30 12:38:07 +0200

    Smallfix: Added some echoes and a command to remove the search index

--- regenerate.sh
+++ regenerate.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 # For postgresql
+echo "-> Remove greenmine DB"
 dropdb greenmine
+echo "-> Create greenmine DB"
 createdb greenmine
 
+echo "-> Remove the search index"
+rm -rf greenmine/search/index/*
+
+
+echo "-> Run syncdb"
 python manage.py syncdb --migrate --noinput --traceback
+echo "-> Load initial user"
 python manage.py loaddata initial_user --traceback
+echo "-> Load initial roles"
 python manage.py loaddata initial_role --traceback
+echo "-> Generate sample data"
 python manage.py sample_data --traceback
+echo "-> Generate initial versions of objects"
 python manage.py createinitialrevisions --traceback
 
