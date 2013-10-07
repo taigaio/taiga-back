3cd8d913fa1a748e1d4017a785840bc1bef6bd44
Jesús Espino <jesus.espino@kaleidos.net>
2013-10-07 17:37:34 +0200

    Adding required=False to milestone generated fields

--- greenmine/projects/milestones/serializers.py
+++ greenmine/projects/milestones/serializers.py
@@ -12,9 +12,9 @@ from . import models
 
 class MilestoneSerializer(serializers.ModelSerializer):
     user_stories = UserStorySerializer(many=True, required=False)
-    closed_points = serializers.FloatField(source='closed_points')
-    client_increment_points = serializers.FloatField(source='client_increment_points')
-    team_increment_points = serializers.FloatField(source='team_increment_points')
+    closed_points = serializers.FloatField(source='closed_points', required=False)
+    client_increment_points = serializers.FloatField(source='client_increment_points', required=False)
+    team_increment_points = serializers.FloatField(source='team_increment_points', required=False)
 
     class Meta:
         model = models.Milestone
