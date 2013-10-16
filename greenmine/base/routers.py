4b7b2727a35cfcb0117b0ba4571da9a0ea81824a
Andrey Antukh <niwi@niwi.be>
2013-10-16 17:13:59 +0200

    Remove old reimplementation of routes.

--- greenmine/base/routers.py
+++ greenmine/base/routers.py
@@ -2,20 +2,9 @@
 
 from rest_framework import routers
 
-# Special router for actions.
-actions_router = routers.Route(url=r'^{prefix}/{methodname}{trailing_slash}$',
-                               mapping={'{httpmethod}': '{methodname}'},
-                               name='{basename}-{methodnamehyphen}',
-                               initkwargs={})
-
 
 class DefaultRouter(routers.DefaultRouter):
-    routes = [
-        routers.DefaultRouter.routes[0],
-        actions_router,
-        routers.DefaultRouter.routes[2],
-        routers.DefaultRouter.routes[1]
-    ]
+    pass
 
 
 __all__ = ["DefaultRouter"]
