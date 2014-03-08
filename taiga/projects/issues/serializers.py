3990e3aa64cff288def07ee36e24026cc15282c0
David Barragán Merino <david.barragan@kaleidos.net>
2014-03-08 19:42:14 +0100

    Remove unnecessary field from IssueSerializer

--- taiga/projects/issues/serializers.py
+++ taiga/projects/issues/serializers.py
@@ -9,14 +9,10 @@ from . import models
 
 class IssueSerializer(serializers.ModelSerializer):
     tags = PickleField(required=False)
-    comment = serializers.SerializerMethodField("get_comment")
     is_closed = serializers.Field(source="is_closed")
 
     class Meta:
         model = models.Issue
-
-    def get_comment(self, obj):
-        return ""
 
 
 class IssueNeighborsSerializer(NeighborsSerializerMixin, IssueSerializer):
