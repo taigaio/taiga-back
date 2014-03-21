d433ed78675cf97322848eaa6e9f4f9d96180c9f
Andrey Antukh <niwi@niwi.be>
2014-03-21 20:39:52 +0100

    Remove production not used setting.

--- settings/development.py
+++ settings/development.py
@@ -4,9 +4,6 @@ from .common import *
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-USE_ETAGS = False
-
-SESSION_ENGINE='django.contrib.sessions.backends.db'
 
 TEMPLATE_CONTEXT_PROCESSORS += [
     "django.core.context_processors.debug",
