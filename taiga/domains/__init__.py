d72a3549279ebf21679444ca6606a952880757a9
Andrey Antukh <niwi@niwi.be>
2014-03-22 16:08:51 +0100

    Fix typo on function alias on domains/__init__.py file.

--- taiga/domains/__init__.py
+++ taiga/domains/__init__.py
@@ -16,12 +16,12 @@ from .base import get_default_domain
 from .base import get_domain_for_domain_name
 from .base import activate
 from .base import deactivate
-from .base import get_default_domain
+from .base import get_active_domain
 from .base import DomainNotFound
 
 __all__ = ["get_default_domain",
            "get_domain_for_domain_name",
            "activate",
            "deactivate",
-           "get_default_domain",
+           "get_active_domain",
            "DomainNotFound"]
