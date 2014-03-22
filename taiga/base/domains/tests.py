8318c928f097a0d97c13c0c87c7f0391d30f93a1
Andrey Antukh <niwi@niwi.be>
2014-03-22 15:52:02 +0100

    Add mission apache license header on main domain modules.

--- taiga/base/domains/tests.py
+++ taiga/base/domains/tests.py
@@ -1,3 +1,17 @@
+# Copyright 2014 Andrey Antukh <niwi@niwi.be>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# y ou may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from django import test
 from django.test.utils import override_settings
 from django.core.exceptions import ImproperlyConfigured
@@ -7,7 +21,6 @@ from django.http import HttpResponse
 from . import base
 from .models import Domain
 from .middleware import DomainsMiddleware
-
 
 
 class DomainCoreTests(test.TestCase):
