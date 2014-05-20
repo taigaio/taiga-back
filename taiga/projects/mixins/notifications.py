2b6423a396c22e0ea6cf4a21de2155a4789d7315
David Barragán Merino <david.barragan@kaleidos.net>
2014-05-20 10:21:29 +0200

    Fix watchers validator like in stable branch

--- taiga/projects/mixins/notifications.py
+++ taiga/projects/mixins/notifications.py
@@ -99,18 +99,11 @@ class WatcherValidationSerializerMixin(object):
         if values:
             project = None
             if "project" in attrs and attrs["project"]:
-                if self.object and attrs["project"] == self.object.project.id:
-                    project = self.object.project
-                else:
-                    project_model = get_model("projects", "Project")
-                    try:
-                        project = project_model.objects.get(project__id=attrs["project"])
-                    except project_model.DoesNotExist:
-                        pass
+                project = attrs["project"]
             elif self.object:
                 project = self.object.project
 
-            if len(values) != get_model("projects", "Membership").objects.filter(project=project,
-                                                                                 user__in=values).count():
+            model_cls = get_model("projects", "Membership")
+            if len(values) != model_cls.objects.filter(project=project, user__in=values).count():
                 raise serializers.ValidationError("Error, some watcher user is not a member of the project")
         return attrs
