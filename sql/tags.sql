270b48dc45ccb122612405eeb9077ca91e09ed1d
Jesús Espino <jesus.espino@kaleidos.net>
2014-05-20 09:35:51 +0200

    Now tags.sql works correctly on vagrant and can be execute multiple times

--- sql/tags.sql
+++ sql/tags.sql
@@ -1,4 +1,4 @@
-CREATE LANGUAGE plpythonu;
+CREATE OR REPLACE LANGUAGE plpython3u;
 
 CREATE OR REPLACE FUNCTION unpickle (data text)
   RETURNS text[]
@@ -7,6 +7,7 @@ AS $$
     import pickle
 
     return pickle.loads(base64.b64decode(data))
-$$ LANGUAGE plpythonu IMMUTABLE;
+$$ LANGUAGE plpython3u IMMUTABLE;
 
+DROP INDEX IF EXISTS issues_unpickle_tags_index;
 CREATE INDEX issues_unpickle_tags_index ON issues_issue USING btree (unpickle(tags));
