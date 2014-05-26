4ec6ba621b8b523db2e870467c574fcf512e0008
Andrey Antukh <niwi@niwi.be>
2014-05-26 17:18:36 +0200

    Make integration tests as functions.

--- tests/integration/test_auth_api.py
+++ tests/integration/test_auth_api.py
@@ -1,50 +1,42 @@
-from unittest import mock
 import pytest
 
 from django.core.urlresolvers import reverse
-
 from .. import factories
 
 pytestmark = pytest.mark.django_db
 
-
-def setup_module(module):
-    module.patcher = mock.patch("taiga.domains.base.get_default_domain",
-                                mock.Mock(return_value=factories.DomainFactory()))
-    module.patcher.start()
-
-def teardown_module(module):
-    module.patcher.stop()
-
-
-class TestPublicRegistration:
-    @classmethod
-    def setup_class(cls):
-        cls.form = {"username": "username", "password": "password", "first_name": "fname",
-                    "last_name": "lname", "email": "user@email.com", "type": "public"}
-
-    def test_respond_201_if_domain_allows_public_registration(self, client):
-        domain = factories.DomainFactory(public_register=True)
-        response = client.post(reverse("auth-register"), self.form, HTTP_X_HOST=domain.domain)
-        assert response.status_code == 201
-
-    def test_respond_400_if_domain_does_not_allow_public_registration(self, client):
-        domain = factories.DomainFactory(public_register=False)
-        response = client.post(reverse("auth-register"), self.form, HTTP_X_HOST=domain.domain)
-        assert response.status_code == 400
+@pytest.fixture
+def register_form():
+   return {"username": "username",
+           "password": "password",
+           "first_name": "fname",
+           "last_name": "lname",
+           "email": "user@email.com",
+           "type": "public"}
 
 
-@pytest.mark.xfail
-class TestPrivateRegistration:
-    @classmethod
-    def setup_class(cls):
-        cls.form = {"username": "username", "password": "password", "first_name": "fname",
-                    "last_name": "lname", "email": "user@email.com", "type": "private",
-                    "existing": "1"}
+def test_respond_201_if_domain_allows_public_registration(client, register_form):
+    response = client.post(reverse("auth-register"), register_form)
+    assert response.status_code == 201
 
-    def test_respond_201_if_domain_allows_public_registration(self, client):
-        domain = factories.DomainFactory(public_register=True)
-        membership = factories.MembershipFactory()
-        headers = {"HTTP_X_HOST": domain.domain}
-        response = client.post(reverse("auth-register"), self.form, **headers)
-        assert response.status_code == 201
+
+def test_respond_400_if_domain_does_not_allow_public_registration(client, register_form):
+    response = client.post(reverse("auth-register"), register_form)
+    assert response.status_code == 400
+
+
+def test_respond_201_if_domain_allows_public_registration(client, register_form):
+    user = factories.UserFactory()
+    membership = factories.MembershipFactory(user=user)
+
+    register_form.update({
+        "type": "private",
+        "existing": "1",
+        "token": membership.token,
+        "username": user.username,
+        "email": user.email,
+        "password": user.username,
+    })
+
+    response = client.post(reverse("auth-register"), register_form)
+    assert response.status_code == 201
