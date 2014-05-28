1531dcea02486bb1a295b9450f7bd16bdb4e9197
Andrey Antukh <niwi@niwi.be>
2014-05-28 18:27:20 +0200

    Cosmetic changes on stars __init__.py file.

--- taiga/projects/stars/__init__.py
+++ taiga/projects/stars/__init__.py
@@ -1,1 +1,12 @@
-from .services import star, unstar, get_fans, get_starred
+from .services import star
+from .services import unstar
+from .services import get_fans
+from .services import get_starred
+from .services import attach_startscount_to_queryset
+
+
+__all__ = ("star",
+           "unstar",
+           "get_fans",
+           "get_starred",
+           "attach_startscount_to_queryset",)
