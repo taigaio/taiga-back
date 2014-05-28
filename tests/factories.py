a998ac0660b10981a51e9f6982306cbb4d13fa3a
Andrey Antukh <niwi@niwi.be>
2014-05-28 12:55:33 +0200

    Remove global imports from test factories.

--- tests/factories.py
+++ tests/factories.py
@@ -1,16 +1,18 @@
 import uuid
 import threading
 
+from django.db.models.loading import get_model
+
 import factory
 from django.conf import settings
 
-import taiga.projects.models
-import taiga.projects.userstories.models
-import taiga.projects.issues.models
-import taiga.projects.milestones.models
-import taiga.projects.stars.models
-import taiga.users.models
-import taiga.userstorage.models
+# import taiga.projects.models
+# import taiga.projects.userstories.models
+# import taiga.projects.issues.models
+# import taiga.projects.milestones.models
+# import taiga.projects.stars.models
+# import taiga.users.models
+# import taiga.userstorage.models
 
 
 class Factory(factory.DjangoModelFactory):
@@ -27,7 +29,7 @@ class Factory(factory.DjangoModelFactory):
 
 
 class ProjectTemplateFactory(Factory):
-    FACTORY_FOR = taiga.projects.models.ProjectTemplate
+    FACTORY_FOR = get_model("projects", "ProjectTemplate")
     FACTORY_DJANGO_GET_OR_CREATE = ("slug", )
 
     name = "Template name"
@@ -44,7 +46,7 @@ class ProjectTemplateFactory(Factory):
 
 
 class ProjectFactory(Factory):
-    FACTORY_FOR = taiga.projects.models.Project
+    FACTORY_FOR = get_model("projects", "Project")
 
     name = factory.Sequence(lambda n: "Project {}".format(n))
     slug = factory.Sequence(lambda n: "project-{}-slug".format(n))
@@ -54,14 +56,14 @@ class ProjectFactory(Factory):
 
 
 class RoleFactory(Factory):
-    FACTORY_FOR = taiga.users.models.Role
+    FACTORY_FOR = get_model("users", "Role")
 
     name = "Tester"
     project = factory.SubFactory("tests.factories.ProjectFactory")
 
 
 class PointsFactory(Factory):
-    FACTORY_FOR = taiga.projects.models.Points
+    FACTORY_FOR = get_model("projects", "Points")
 
     name = factory.Sequence(lambda n: "Points {}".format(n))
     value = 2
@@ -69,7 +71,7 @@ class PointsFactory(Factory):
 
 
 class RolePointsFactory(Factory):
-    FACTORY_FOR = taiga.projects.userstories.models.RolePoints
+    FACTORY_FOR = get_model("userstories", "RolePoints")
 
     user_story = factory.SubFactory("tests.factories.UserStoryFactory")
     role = factory.SubFactory("tests.factories.RoleFactory")
@@ -77,7 +79,7 @@ class RolePointsFactory(Factory):
 
 
 class UserFactory(Factory):
-    FACTORY_FOR = taiga.users.models.User
+    FACTORY_FOR = get_model("users", "User")
 
     username = factory.Sequence(lambda n: "user{}".format(n))
     email = factory.LazyAttribute(lambda obj: '%s@email.com' % obj.username)
@@ -85,7 +87,7 @@ class UserFactory(Factory):
 
 
 class MembershipFactory(Factory):
-    FACTORY_FOR = taiga.projects.models.Membership
+    FACTORY_FOR = get_model("projects", "Membership")
 
     token = factory.LazyAttribute(lambda obj: str(uuid.uuid1()))
     project = factory.SubFactory("tests.factories.ProjectFactory")
@@ -94,7 +96,7 @@ class MembershipFactory(Factory):
 
 
 class StorageEntryFactory(Factory):
-    FACTORY_FOR = taiga.userstorage.models.StorageEntry
+    FACTORY_FOR = get_model("userstorage", "StorageEntry")
 
     owner = factory.SubFactory("tests.factories.UserFactory")
     key = factory.Sequence(lambda n: "key-{}".format(n))
@@ -102,7 +104,7 @@ class StorageEntryFactory(Factory):
 
 
 class UserStoryFactory(Factory):
-    FACTORY_FOR = taiga.projects.userstories.models.UserStory
+    FACTORY_FOR = get_model("userstories", "UserStory")
 
     ref = factory.Sequence(lambda n: n)
     project = factory.SubFactory("tests.factories.ProjectFactory")
@@ -111,7 +113,7 @@ class UserStoryFactory(Factory):
 
 
 class MilestoneFactory(Factory):
-    FACTORY_FOR = taiga.projects.milestones.models.Milestone
+    FACTORY_FOR = get_model("milestones", "Milestone")
 
     name = factory.Sequence(lambda n: "Milestone {}".format(n))
     owner = factory.SubFactory("tests.factories.UserFactory")
@@ -119,7 +121,7 @@ class MilestoneFactory(Factory):
 
 
 class IssueFactory(Factory):
-    FACTORY_FOR = taiga.projects.issues.models.Issue
+    FACTORY_FOR = get_model("issues", "Issue")
 
     subject = factory.Sequence(lambda n: "Issue {}".format(n))
     owner = factory.SubFactory("tests.factories.UserFactory")
@@ -132,42 +134,42 @@ class IssueFactory(Factory):
 
 
 class IssueStatusFactory(Factory):
-    FACTORY_FOR = taiga.projects.models.IssueStatus
+    FACTORY_FOR = get_model("projects", "IssueStatus")
 
     name = factory.Sequence(lambda n: "Issue Status {}".format(n))
     project = factory.SubFactory("tests.factories.ProjectFactory")
 
 
 class SeverityFactory(Factory):
-    FACTORY_FOR = taiga.projects.models.Severity
+    FACTORY_FOR = get_model("projects", "Severity")
 
     name = factory.Sequence(lambda n: "Severity {}".format(n))
     project = factory.SubFactory("tests.factories.ProjectFactory")
 
 
 class PriorityFactory(Factory):
-    FACTORY_FOR = taiga.projects.models.Priority
+    FACTORY_FOR = get_model("projects", "Priority")
 
     name = factory.Sequence(lambda n: "Priority {}".format(n))
     project = factory.SubFactory("tests.factories.ProjectFactory")
 
 
 class IssueTypeFactory(Factory):
-    FACTORY_FOR = taiga.projects.models.IssueType
+    FACTORY_FOR = get_model("projects", "IssueType")
 
     name = factory.Sequence(lambda n: "Issue Type {}".format(n))
     project = factory.SubFactory("tests.factories.ProjectFactory")
 
 
 class FanFactory(Factory):
-    FACTORY_FOR = taiga.projects.stars.models.Fan
+    FACTORY_FOR = get_model("stars", "Fan")
 
     project = factory.SubFactory("tests.factories.ProjectFactory")
     user = factory.SubFactory("tests.factories.UserFactory")
 
 
 class StarsFactory(Factory):
-    FACTORY_FOR = taiga.projects.stars.models.Stars
+    FACTORY_FOR = get_model("stars", "Stars")
 
     project = factory.SubFactory("tests.factories.ProjectFactory")
     count = 0
