aa7ca6e3fcb97f58191eb5ef1f717f1b16125cb3
Anler Hp <anler86@gmail.com>
2014-06-02 16:57:48 +0200

    Reconnect disconnected signals in integration tests

--- tests/integration/test_neighbors.py
+++ tests/integration/test_neighbors.py
@@ -9,11 +9,15 @@ from taiga.base.utils.db import filter_by_tags
 from taiga.base import neighbors as n
 
 from .. import factories as f
-from ..utils import disconnect_signals
+from ..utils import disconnect_signals, reconnect_signals
 
 
 def setup_module():
     disconnect_signals()
+
+
+def teardown_module():
+    reconnect_signals()
 
 
 class TestGetAttribute:
