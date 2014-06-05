236d03777021e218f5432a43e36677a1462ee13c
Jesús Espino <jesus.espino@kaleidos.net>
2014-06-05 12:31:56 +0200

    Fixed problem with verbose_name template function registration

--- taiga/projects/history/templatetags/functions.py
+++ taiga/projects/history/templatetags/functions.py
@@ -14,9 +14,9 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from django_jinja.base import Library
+from django_jinja import library
 
-register = Library()
+register = library.Library()
 
 
 @register.global_function
