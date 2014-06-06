102f3768544c180395b5b044ad0c0bf628d5f89a
David Barragán Merino <david.barragan@kaleidos.net>
2014-06-06 09:16:00 +0200

    Fix import

--- taiga/projects/votes/serializers.py
+++ taiga/projects/votes/serializers.py
@@ -1,11 +1,11 @@
-from django.contrib.auth import get_user_model
-
 from rest_framework import serializers
+
+from taiga.users.models import User
 
 
 class VoterSerializer(serializers.ModelSerializer):
     full_name = serializers.CharField(source='get_full_name', required=False)
 
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ('id', 'username', 'first_name', 'last_name', 'full_name')
