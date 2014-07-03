9b8531d56212b70129ebb92b06251b8bfd5baf6e
Anler Hp <anler86@gmail.com>
2014-07-03 12:35:06 +0200

    Removing unnecessary package (included by mistake)

--- taiga/services/celery.py
+++
@@ -1,8 +0,0 @@
-from celery import Celery
-
-from django.conf import settings
-
-app = Celery('taiga')
-
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
