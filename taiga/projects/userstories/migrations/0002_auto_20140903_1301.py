775e724c39665da4542eb46ab8c486810b7c9562
Andrey Antukh <niwi@niwi.be>
2014-09-03 15:49:47 +0200

    Set RolePoints.points relation nullable.

---
+++ taiga/projects/userstories/migrations/0002_auto_20140903_1301.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('userstories', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='rolepoints',
+            name='points',
+            field=models.ForeignKey(related_name='role_points', to='projects.Points', null=True, verbose_name='points'),
+        ),
+    ]
