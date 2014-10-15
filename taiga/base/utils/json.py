480baa3b948818059c0d31c758398ddcd58b1bbb
Andrey Antukh <niwi@niwi.be>
2014-10-15 17:29:20 +0200

    Auto coerce bytes to string on own json module.

--- taiga/base/utils/json.py
+++ taiga/base/utils/json.py
@@ -16,13 +16,15 @@
 
 import json
 from rest_framework.utils import encoders
+from django.utils.encoding import force_text
 
 
 def dumps(data, ensure_ascii=True, encoder_class=encoders.JSONEncoder):
     return json.dumps(data, cls=encoder_class, indent=None, ensure_ascii=ensure_ascii)
 
-
 def loads(data):
+    if isinstance(data, bytes):
+        data = force_text(data)
     return json.loads(data)
 
 # Some backward compatibility that should
