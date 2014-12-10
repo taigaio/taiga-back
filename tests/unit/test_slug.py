ce88a6007355008973d81cd5da52dfa30d53730c
David Barragán Merino <david.barragan@kaleidos.net>
2014-12-10 21:02:43 +0100

    tg-1783 #ready-for-test - Fix wiki links compatibility

--- tests/unit/test_slug.py
+++ tests/unit/test_slug.py
@@ -18,8 +18,22 @@
 from taiga.projects.models import Project
 from taiga.users.models import User
 
+from taiga.base.utils.slug import slugify
+
 import pytest
 pytestmark = pytest.mark.django_db
+
+
+def test_slugify_1():
+    assert slugify("漢字") == "han-zi"
+
+
+def test_slugify_2():
+    assert slugify("TestExamplePage") == "testexamplepage"
+
+
+def test_slugify_3():
+    assert slugify(None) == ""
 
 
 def test_project_slug_with_special_chars():
