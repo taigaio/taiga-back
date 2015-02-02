c6b1906378ac89cac47051c0c9ec6c5f0a16ba02
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2015-02-02 09:40:11 +0100

    Adding lost webhooks migration

---
+++ taiga/webhooks/migrations/0004_auto_20150202_0834.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('webhooks', '0003_auto_20150122_1021'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='webhook',
+            options={'ordering': ['name', '-id']},
+        ),
+        migrations.AlterModelOptions(
+            name='webhooklog',
+            options={'ordering': ['-created', '-id']},
+        ),
+    ]
