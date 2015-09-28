f4b11b26fbef0c83124ec3783f8c0c7b11ec0f6d
David Barragán Merino <david.barragan@kaleidos.net>
2015-09-28 10:59:32 +0200

    Minor improvements in the command 'generate_sha1'

--- taiga/projects/attachments/management/commands/generate_sha1.py
+++ taiga/projects/attachments/management/commands/generate_sha1.py
@@ -3,10 +3,17 @@ from django.db import transaction
 
 from taiga.projects.attachments.models import Attachment
 
+import logging
+logger = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
-
     @transaction.atomic
     def handle(self, *args, **options):
-        for attachment in Attachment.objects.all():
+        total = rest = Attachment.objects.all().count()
+
+        for attachment in Attachment.objects.all().order_by("id"):
             attachment.save()
+
+            rest -= 1
+            logger.debug("[{} / {} remaining] - Generate sha1 for attach {}".format(rest, total, attachment.id))
