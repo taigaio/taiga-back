93412840c9f8173f2579aac912ca17dc28fd6222
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2016-01-04 12:50:49 +0100

    Issue #3283: Taiga 1.8.0: HTTP error 500 while trying to delete project as admin.

--- taiga/projects/models.py
+++ taiga/projects/models.py
@@ -308,6 +308,31 @@ class Project(ProjectDefaults, TaggedMixin, models.Model):
         notify_policy = get_notify_policy(self, user)
         set_notify_policy_level_to_ignore(notify_policy)
 
+    def delete_related_content(self):
+        from taiga.events.apps import connect_events_signals, disconnect_events_signals
+        from taiga.projects.tasks.apps import connect_all_tasks_signals, disconnect_all_tasks_signals
+        from taiga.projects.userstories.apps import connect_all_userstories_signals, disconnect_all_userstories_signals
+        from taiga.projects.issues.apps import connect_all_issues_signals, disconnect_all_issues_signals
+        from taiga.projects.apps import connect_memberships_signals, disconnect_memberships_signals
+        
+        disconnect_events_signals()
+        disconnect_all_issues_signals()
+        disconnect_all_tasks_signals()
+        disconnect_all_userstories_signals()
+        disconnect_memberships_signals()
+
+        try:
+            self.tasks.all().delete()
+            self.user_stories.all().delete()
+            self.issues.all().delete()
+            self.memberships.all().delete()
+            self.roles.all().delete()
+        finally:
+            connect_events_signals()
+            connect_all_issues_signals()
+            connect_all_tasks_signals()
+            connect_all_userstories_signals()
+            connect_memberships_signals()
 
 class ProjectModulesConfig(models.Model):
     project = models.OneToOneField("Project", null=False, blank=False,
