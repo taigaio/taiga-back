f992d372dfe106d1a9fa3e24d0f90058b242afb6
David Barragán Merino <david.barragan@kaleidos.net>
2016-01-04 08:50:03 +0100

    Update license message

--- taiga/webhooks/tasks.py
+++ taiga/webhooks/tasks.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2013 Andrey Antukh <niwi@niwi.be>
-# Copyright (C) 2014-2015 Jesús Espino <jespinog@gmail.com>
-# Copyright (C) 2014-2015 David Barragán <bameda@dbarragan.com>
+# Copyright (C) 2014-2016 Jesús Espino <jespinog@gmail.com>
+# Copyright (C) 2014-2016 David Barragán <bameda@dbarragan.com>
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
