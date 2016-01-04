7eb138c200edd4ce7e068b58736d37da89bbf87b
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2016-01-04 15:01:17 +0100

    Adding command to send notification messages

---
+++ taiga/events/management/commands/emit_notification_message.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2014-2016 Andrey Antukh <niwi@niwi.be>
+# Copyright (C) 2014-2016 Jesús Espino <jespinog@gmail.com>
+# Copyright (C) 2014-2016 David Barragán <bameda@dbarragan.com>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.core.management.base import BaseCommand
+
+from taiga.events.events import emit_event
+
+class Command(BaseCommand):
+    help = 'Send a notification message to the current users'
+
+    def add_arguments(self, parser):
+        parser.add_argument("title",  help="The title of the message.")
+        parser.add_argument("description",  help="The description of the message.")
+
+    def handle(self, **options):
+        data = {
+            "title": options["title"],
+            "desc": options["description"],
+        }
+        routing_key = "notifications"
+        emit_event(data, routing_key)
