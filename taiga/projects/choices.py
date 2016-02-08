7ec0ecfc334bc657be743dc29fd0c1c378aab488
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2016-02-08 14:12:58 +0100

    Fixing blocked by owner leave error message

--- taiga/projects/choices.py
+++ taiga/projects/choices.py
@@ -29,5 +29,5 @@ BLOCKED_BY_STAFF = "blocked-by-staff"
 BLOCKED_BY_OWNER_LEAVING = "blocked-by-owner-leaving"
 BLOCKING_CODES = [
     (BLOCKED_BY_STAFF, _("This project was blocked by staff")),
-    (BLOCKED_BY_OWNER_LEAVING, _("This project was because the owner left"))
+    (BLOCKED_BY_OWNER_LEAVING, _("This project was blocked because the owner left"))
 ]
