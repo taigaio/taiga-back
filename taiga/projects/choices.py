03674f8962fc3ae5fccf74166270013f31986a0b
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2016-05-31 12:38:03 +0200

    Improving project deletion

--- taiga/projects/choices.py
+++ taiga/projects/choices.py
@@ -29,8 +29,11 @@ VIDEOCONFERENCES_CHOICES = (
 BLOCKED_BY_NONPAYMENT = "blocked-by-nonpayment"
 BLOCKED_BY_STAFF = "blocked-by-staff"
 BLOCKED_BY_OWNER_LEAVING = "blocked-by-owner-leaving"
+BLOCKED_BY_DELETING = "blocked-by-deleting"
+
 BLOCKING_CODES = [
     (BLOCKED_BY_NONPAYMENT, _("This project is blocked due to payment failure")),
     (BLOCKED_BY_STAFF, _("This project is blocked by admin staff")),
-    (BLOCKED_BY_OWNER_LEAVING, _("This project is blocked because the owner left"))
+    (BLOCKED_BY_OWNER_LEAVING, _("This project is blocked because the owner left")),
+    (BLOCKED_BY_DELETING, _("This project is blocked while it's deleted"))
 ]
