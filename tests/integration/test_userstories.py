f3176f4b5ac31bd8ea970429377d67f3dea9461a
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2016-06-02 08:50:06 +0200

    Detecting errors properly on points

--- tests/integration/test_userstories.py
+++ tests/integration/test_userstories.py
@@ -248,33 +248,42 @@ def test_update_userstory_points(client):
     f.PointsFactory.create(project=project, value=1)
     points3 = f.PointsFactory.create(project=project, value=2)
 
-    us = f.UserStoryFactory.create(project=project, owner=user1, status__project=project, milestone__project=project)
+    us = f.UserStoryFactory.create(project=project,owner=user1, status__project=project,
+                                   milestone__project=project)
     usdata = UserStorySerializer(us).data
 
     url = reverse("userstories-detail", args=[us.pk])
 
     client.login(user1)
 
-    # Api should ignore invalid values
+    # invalid role
     data = {}
     data["version"] = usdata["version"]
     data["points"] = copy.copy(usdata["points"])
-    data["points"].update({'2000': points3.pk})
+    data["points"].update({"222222": points3.pk})
 
     response = client.json.patch(url, json.dumps(data))
-    assert response.status_code == 200, str(response.content)
-    assert response.data["points"] == usdata['points']
+    assert response.status_code == 400
+
+    # invalid point
+    data = {}
+    data["version"] = usdata["version"]
+    data["points"] = copy.copy(usdata["points"])
+    data["points"].update({str(role1.pk): "999999"})
+
+    response = client.json.patch(url, json.dumps(data))
+    assert response.status_code == 400
 
     # Api should save successful
     data = {}
-    data["version"] = usdata["version"] + 1
+    data["version"] = usdata["version"]
     data["points"] = copy.copy(usdata["points"])
     data["points"].update({str(role1.pk): points3.pk})
 
     response = client.json.patch(url, json.dumps(data))
     us = models.UserStory.objects.get(pk=us.pk)
     usdatanew = UserStorySerializer(us).data
-    assert response.status_code == 200
+    assert response.status_code == 200, str(response.content)
     assert response.data["points"] == usdatanew['points']
     assert response.data["points"] != usdata['points']
 
