9d3e53f3c5384b499feee6eca131f984912c125a
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2016-10-13 10:48:05 +0200

    Fixing and refactoring by_ref api endpoints

--- taiga/projects/tasks/api.py
+++ taiga/projects/tasks/api.py
@@ -28,6 +28,7 @@ from taiga.base.api.mixins import BlockedByProjectMixin
 from taiga.base.utils import json
 from taiga.projects.history.mixins import HistoryResourceMixin
 from taiga.projects.milestones.models import Milestone
+from taiga.projects.mixins.by_ref import ByRefMixin
 from taiga.projects.models import Project, TaskStatus
 from taiga.projects.notifications.mixins import WatchedResourceMixin, WatchersViewSetMixin
 from taiga.projects.occ import OCCResourceMixin
@@ -44,9 +45,8 @@ from . import validators
 from . import utils as tasks_utils
 
 
-class TaskViewSet(OCCResourceMixin, VotedResourceMixin, HistoryResourceMixin,
-                  WatchedResourceMixin, TaggedResourceMixin, BlockedByProjectMixin,
-                  ModelCrudViewSet):
+class TaskViewSet(OCCResourceMixin, VotedResourceMixin, HistoryResourceMixin, WatchedResourceMixin,
+                  ByRefMixin, TaggedResourceMixin, BlockedByProjectMixin, ModelCrudViewSet):
     validator_class = validators.TaskValidator
     queryset = models.Task.objects.all()
     permission_classes = (permissions.TaskPermission,)
@@ -219,21 +219,6 @@ class TaskViewSet(OCCResourceMixin, VotedResourceMixin, HistoryResourceMixin,
             "tags": self.filter_queryset(queryset)
         }
         return response.Ok(services.get_tasks_filters_data(project, querysets))
-
-    @list_route(methods=["GET"])
-    def by_ref(self, request):
-        retrieve_kwargs = {
-            "ref": request.QUERY_PARAMS.get("ref", None)
-        }
-        project_id = request.QUERY_PARAMS.get("project", None)
-        if project_id is not None:
-            retrieve_kwargs["project_id"] = project_id
-
-        project_slug = request.QUERY_PARAMS.get("project__slug", None)
-        if project_slug is not None:
-            retrieve_kwargs["project__slug"] = project_slug
-
-        return self.retrieve(request, **retrieve_kwargs)
 
     @list_route(methods=["GET"])
     def csv(self, request):
