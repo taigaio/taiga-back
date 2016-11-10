ba76c9431f118ec633b5f28fbcf50084a34d4572
David Barragán Merino <david.barragan@kaleidos.net>
2016-11-10 20:35:34 +0100

    [Backport] Throw a signal when users change their email adress

--- taiga/users/signals.py
+++ taiga/users/signals.py
@@ -19,4 +19,5 @@
 import django.dispatch
 
 
+user_change_email = django.dispatch.Signal(providing_args=["user", "old_email", "new_email"])
 user_cancel_account = django.dispatch.Signal(providing_args=["user", "request_data"])
