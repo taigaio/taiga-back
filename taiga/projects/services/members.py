7bf5ad1a4bc7823ef5d1e96bc1f3f92b3295a33c
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2017-01-02 10:01:24 +0100

    Limiting max number of pending memberships

--- taiga/projects/services/members.py
+++ taiga/projects/services/members.py
@@ -20,6 +20,7 @@ from taiga.base.exceptions import ValidationError
 from taiga.base.utils import db, text
 from taiga.users.models import User
 
+from django.conf import settings
 from django.core.validators import validate_email
 from django.utils.translation import ugettext as _
 
@@ -142,5 +143,9 @@ def check_if_project_can_have_more_memberships(project, total_new_memberships):
 
     if max_memberships is not None and total_memberships > max_memberships:
         return False, error_members_exceeded
+
+    if project.memberships.filter(user=None).count() + total_new_memberships > settings.MAX_PENDING_MEMBERSHIPS:
+        error_pending_memberships_exceeded = _("You have reached the current limit of pending memberships")
+        return False, error_pending_memberships_exceeded
 
     return True, None
