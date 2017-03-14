363c9432f52f484caa52e7fae0cab3c2f92aeb13
David Barragán Merino <david.barragan@kaleidos.net>
2017-03-14 12:57:01 +0100

    Allow to resolve wikipages with references endpoint

--- taiga/projects/references/validators.py
+++ taiga/projects/references/validators.py
@@ -28,8 +28,8 @@ class ResolverValidator(validators.Validator):
     us = serializers.IntegerField(required=False)
     task = serializers.IntegerField(required=False)
     issue = serializers.IntegerField(required=False)
-    ref = serializers.IntegerField(required=False)
     wikipage = serializers.CharField(max_length=512, required=False)
+    ref = serializers.CharField(max_length=512, required=False)
 
     def validate(self, attrs):
         if "ref" in attrs:
@@ -41,5 +41,7 @@ class ResolverValidator(validators.Validator):
                 raise ValidationError("'task' param is incompatible with 'ref' in the same request")
             if "issue" in attrs:
                 raise ValidationError("'issue' param is incompatible with 'ref' in the same request")
+            if "wikipage" in attrs:
+                raise ValidationError("'wikipage' param is incompatible with 'ref' in the same request")
 
         return attrs
