ee3e94d2e7fb28acb476a5abf164d3ca4db112b5
Jesús Espino <jespinog@gmail.com>
2018-02-07 10:48:04 +0100

    [backport] Add advisory lock to send_notifications

--- taiga/projects/notifications/management/commands/send_notifications.py
+++ taiga/projects/notifications/management/commands/send_notifications.py
@@ -23,12 +23,18 @@ from taiga.base.utils.iterators import iter_queryset
 from taiga.projects.notifications.models import HistoryChangeNotification
 from taiga.projects.notifications.services import send_sync_notifications
 
+from django_pglocks import advisory_lock
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        qs = HistoryChangeNotification.objects.all()
-        for change_notification in iter_queryset(qs, itersize=100):
-            try:
-                send_sync_notifications(change_notification.pk)
-            except HistoryChangeNotification.DoesNotExist:
-                pass
+        with advisory_lock("send-notifications-command", wait=False) as acquired:
+            if acquired:
+                qs = HistoryChangeNotification.objects.all()
+                for change_notification in iter_queryset(qs, itersize=100):
+                    try:
+                        send_sync_notifications(change_notification.pk)
+                    except HistoryChangeNotification.DoesNotExist:
+                        pass
+            else:
+                print("Other process already running")
