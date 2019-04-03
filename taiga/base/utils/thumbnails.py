a93ba9e5e4fa0718c10550889aa0dee131aace9b
Álex Hermida <alexhermida@gmail.com>
2019-04-03 22:08:17 +0200

    Avoid getting thumbnail from non image files

--- taiga/base/utils/thumbnails.py
+++ taiga/base/utils/thumbnails.py
@@ -75,7 +75,7 @@ def get_thumbnail(file_obj, thumbnailer_size):
         relative_name = file_obj.name
 
     source_extension = os.path.splitext(relative_name)[1][1:]
-    if source_extension == "tiff":
+    if source_extension not in ('png', 'svg', 'gif', 'bmp', 'jpeg', 'jpg'):
         return None
 
     try:
