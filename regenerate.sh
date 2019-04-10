4c52a191e63809ec6c9cd5f3a7619f4872cabd83
Álex Hermida <alexhermida@gmail.com>
2019-04-10 13:50:17 +0200

    Specify database in regenrate script

--- regenerate.sh
+++ regenerate.sh
@@ -19,11 +19,16 @@ if $show_answer ; then
 	fi
 fi
 
+echo "Specify database name:"
+read database
+if [ -z "$database" ]; then
+    exit 1
+fi
 
-echo "-> Remove taiga DB"
-dropdb taiga
+echo "-> Remove taiga DB" $database
+dropdb $database
 echo "-> Create taiga DB"
-createdb taiga
+createdb $database
 
 echo "-> Load migrations"
 python manage.py migrate
