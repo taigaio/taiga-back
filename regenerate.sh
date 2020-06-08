5fa9947b863b614eb41872e7d6b62223ed32b35c
Miguel Gonzalez <migonzalvar@gmail.com>
2020-06-08 16:40:59 +0200

    Add createcachetable

--- regenerate.sh
+++ regenerate.sh
@@ -32,6 +32,7 @@ createdb $database
 
 echo "-> Load migrations"
 python manage.py migrate
+python manage.py createcachetable
 echo "-> Load initial user (admin/123123)"
 python manage.py loaddata initial_user --traceback
 echo "-> Load initial project_templates (scrum/kanban)"
