7fe43954098f0d46c809bb36ab954064363d7199
Yamila Moreno <yamila.ms@gmail.com>
2020-09-28 12:25:59 +0200

    Fix entrypoint with initial data

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,6 +5,11 @@ set -euo pipefail
 echo Executing pending migrations
 python manage.py migrate
 
+# Load initial user and templates (if they don't exist)
+echo "Load initial user and templates (if they dont exist)"
+python manage.py loaddata initial_user
+python manage.py loaddata initial_project_templates
+
 # Start Taiga processes
 echo Starting Taiga API...
 exec gunicorn taiga.wsgi:application \
