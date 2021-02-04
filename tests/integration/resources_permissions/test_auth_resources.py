9f5e25c1eda9167d98baad23b4623b6a4ee39f24
David Barragán Merino <david.barragan@kaleidos.net>
2021-02-04 13:16:11 +0100

    test(auth): fix and add test for short password

--- tests/integration/resources_permissions/test_auth_resources.py
+++ tests/integration/resources_permissions/test_auth_resources.py
@@ -48,7 +48,7 @@ def test_auth_create(client):
     assert result.status_code == 200
 
 
-def test_auth_action_register(client, settings):
+def test_auth_action_register_with_short_password(client, settings):
     settings.PUBLIC_REGISTER_ENABLED = True
     url = reverse('auth-register')
 
@@ -62,4 +62,21 @@ def test_auth_action_register(client, settings):
     })
 
     result = client.post(url, register_data, content_type="application/json")
-    assert result.status_code == 201
+    assert result.status_code == 400, result.json()
+
+
+def test_auth_action_register(client, settings):
+    settings.PUBLIC_REGISTER_ENABLED = True
+    url = reverse('auth-register')
+
+    register_data = json.dumps({
+        "type": "public",
+        "username": "test",
+        "password": "test123",
+        "full_name": "test123",
+        "email": "test@test.com",
+        "accepted_terms": True,
+    })
+
+    result = client.post(url, register_data, content_type="application/json")
+    assert result.status_code == 201, result.json()
