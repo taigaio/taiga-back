d03f6d275204645979da745c275943668048b061
teresa.delatorre <teresa.delatorre@kaleidos.net>
2021-04-13 13:01:39 +0200

    Apply license change to MIT based

--- scripts/manage_translations.py
+++ scripts/manage_translations.py
@@ -1,21 +1,4 @@
 #!/usr/bin/env python
-# Copyright (C) 2014-present Taiga Agile LLC
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
-#
 # NOTE: This script is based on django's manage_translations.py script
 #       (https://github.com/django/django/blob/master/scripts/manage_translations.py)
 #
@@ -38,7 +21,6 @@
 # for Spanish in contrib.admin, run:
 #
 #  $ python scripts/manage_translations.py lang_stats --language=es --resources=taiga
-
 
 import os, errno
 from argparse import ArgumentParser
