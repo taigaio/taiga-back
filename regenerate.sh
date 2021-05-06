0faf09fb6aa0597b51e2fcc92f3ef706d1b7241a
Yamila Moreno <yamila.ms@gmail.com>
2021-05-06 12:13:26 +0200

    (chore) fix shebang

--- regenerate.sh
+++ regenerate.sh
@@ -1,10 +1,10 @@
+#!/bin/bash
+
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 # Copyright (c) 2021-present Kaleidos Ventures SL
-
-#!/bin/bash
 
 show_answer=true
 while [ $# -gt 0 ]; do
--- scripts/generate_fixtures_initial_project_templates.sh
+++ scripts/generate_fixtures_initial_project_templates.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
+
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 # Copyright (c) 2021-present Kaleidos Ventures SL
 
-#!/bin/bash
 python ./manage.py dumpdata --format json \
                             --indent 4 \
                             --output './taiga/projects/fixtures/initial_project_templates.json' \
