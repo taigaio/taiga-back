34278dae3c55aab605e200412933cf0d48c07baf
Nikita Bondarenko <nikabc07@gmail.com>
2022-08-15 02:13:29 +0300

    added sentry-sdk to requirements.in

--- settings/common.py
+++ settings/common.py
@@ -678,3 +678,22 @@ if "test" in sys.argv:
     print("Try: \033[1;33mpy.test\033[0m")
     sys.exit(0)
 # NOTE: DON'T INSERT MORE SETTINGS AFTER THIS LINE
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://31d40623fc5547e1a79547890095525d@o1360724.ingest.sentry.io/6648994",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
