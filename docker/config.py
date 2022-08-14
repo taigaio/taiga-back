14dddea4416902af4091d09f0758aa7509de7557
Nikita Bondarenko <nikabc07@gmail.com>
2022-08-15 02:11:26 +0300

    added TAIGA_SENTRY_DSN in docker/config.py

--- docker/config.py
+++ docker/config.py
@@ -190,3 +190,24 @@ if ENABLE_TRELLO_IMPORTER:
         "api_key": os.getenv('TRELLO_IMPORTER_API_KEY'),
         "secret_key": os.getenv('TRELLO_IMPORTER_SECRET_KEY')
     }
+
+TAIGA_SENTRY_DSN = os.getenv('TAIGA_SENTRY_DSN')
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn=TAIGA_SENTRY_DSN,
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
