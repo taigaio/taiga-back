09ee6a246455086f961fcbe69c0bf3afa7aa5f94
CharlieFromTaiga <carlos.letamendia@revelio.dev>
2025-11-21 11:24:59 +0100

    Update dependencies

--- settings/common.py
+++ settings/common.py
@@ -362,7 +362,6 @@ INSTALLED_APPS = [
     "django_jinja.contrib._humanize",
     "sr",
     "easy_thumbnails",
-    "raven.contrib.django.raven_compat",
 ]
 
 WSGI_APPLICATION = "taiga.wsgi.application"
