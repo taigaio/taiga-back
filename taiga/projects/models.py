4fb34d9663e013826a7e16a6994742cfa043038d
Sergio Alonso <sergio.alonso@revelio.dev>
2025-12-01 13:13:49 +0100

    Add archived_code field to Project model

--- taiga/projects/models.py
+++ taiga/projects/models.py
@@ -250,6 +250,11 @@ class Project(ProjectDefaults, TaggedMixin, TagsColorsMixin, models.Model):
     blocked_code = models.CharField(null=True, blank=True, max_length=255,
                                     choices=choices.BLOCKING_CODES + settings.EXTRA_BLOCKING_CODES,
                                     default=None, verbose_name=_("blocked code"))
+
+    archived_code = models.CharField(null=True, blank=True, max_length=255,
+                                        choices=choices.ArchivedCode.choices + settings.EXTRA_ARCHIVED_CODES,
+                                        default=None,
+                                        verbose_name=_("Archived code"))
     # Totals:
     totals_updated_datetime = models.DateTimeField(null=False, blank=False, auto_now_add=True,
                                                    verbose_name=_("updated date time"), db_index=True)
