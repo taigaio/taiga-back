ee5ba46aa0ceb6a516d10ff495c817f58b8c8fa4
s0mina <github@somina.dev>
2026-04-20 22:40:57 -0600

    Update async_entrypoint.sh
    arm doesnt support gosu, switched to su-exec

--- docker/async_entrypoint.sh
+++ docker/async_entrypoint.sh
@@ -14,7 +14,7 @@ chown -R taiga:taiga /taiga-back
 
 # Start Celery processes
 echo Starting Celery...
-exec gosu taiga celery -A taiga.celery worker -B \
+exec su-exec taiga celery -A taiga.celery worker -B \
     --concurrency 4 \
     -l INFO \
     "$@"
